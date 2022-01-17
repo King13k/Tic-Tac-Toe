5d6a6ee074a154cb619e143911e593e97ee1df50
Kevin King <kevinturingking@gmail.com>
2022-01-17 12:51:43 -0700

    Add player turn header

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // querySelectors
 var boardSpaces = document.querySelectorAll('.square');
 var gameBoard = document.querySelector('.game-board');
+var playerTurn = document.querySelector('.current-turn');
 
 //Event Listeners
 gameBoard.addEventListener('click', placeToken);
@@ -11,14 +12,22 @@ var newGame = new Game();
 function placeToken(event) {
   var clickedSquare = event.target;
   var squareId = event.target.id;
-  if(!clickedSquare.innerHTML) {
+  if (!clickedSquare.innerHTML) {
     clickedSquare.innerHTML = `${newGame.currentPlayer.token}`;
     newGame.updateSquare(squareId);
     newGame.switchTurns();
     console.log(newGame.currentPlayer)
     newGame.turnsTaken += 1;
+    showCurrentPlayer();
     return;
-
   }
-
 }
+
+ function showCurrentPlayer() {
+   if (newGame.sunTurn) {
+     playerTurn.innerHTML = `<h1>${newGame.currentPlayer.token}, Your Turn</h1>`
+   } else if (newGame.moonTurn) {
+     playerTurn.innerHTML = `<h1>${newGame.currentPlayer.token}, Your Turn</h1>`
+   }
+
+ }
