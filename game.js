38647067660ecba26a66f003af02c91af4328fab
Kevin King <kevinturingking@gmail.com>
2022-01-17 10:40:35 -0700

    add updateSquare function to game.js file

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
+class Game {
+  constructor() {
+    this.player1 = new Player("sun","🌝",1);
+    this.player2 = new Player("moon","🌚",2);
+    this.turnsTaken = 0;
+    this.sunTurn = true;
+    this.moonTurn = false;
+    this.currentPlayer = this.player1;
+    this.squares = ['','','','','','','','','']
+    // this.squares is for win condition
+  }
 
+  updateSquare(squareId) {
+    var currentSquare = squareId;
+    this.squares[currentSquare] = this.currentPlayer.token;
+  }
+}
